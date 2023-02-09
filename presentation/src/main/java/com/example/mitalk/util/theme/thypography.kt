9825c2b28c65facbd27db4002d7a7540d62fc036
MIDASIT\kjh1007 <kimjjun1007@gmail.com>
2023-02-09 16:02:42 +0900

    feat: font

---
+++ presentation/src/main/java/com/example/mitalk/util/theme/thypography.kt
@@ -0,0 +1,21 @@
+package com.example.mitalk.util.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.mitalk.R
+
+internal val notoSansKR = FontFamily(
+    Font(R.font.noto_sans_kr_black, FontWeight.Black),
+    Font(R.font.noto_sans_kr_bold, FontWeight.Bold),
+    Font(R.font.noto_sans_kr_light, FontWeight.Light),
+    Font(R.font.noto_sans_kr_medium, FontWeight.Medium),
+    Font(R.font.noto_sans_kr_regular, FontWeight.Normal),
+    Font(R.font.noto_sans_kr_thin, FontWeight.Thin),
+)
+
+internal val gmartketSans = FontFamily(
+    Font(R.font.gmarket_sans_light, FontWeight.Light),
+    Font(R.font.gmarket_sans_medium, FontWeight.Medium),
+    Font(R.font.gmarket_sans_bold, FontWeight.Bold)
+)
