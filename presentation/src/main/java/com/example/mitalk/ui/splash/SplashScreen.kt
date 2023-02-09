003d882e3a261983ef7952f5359efc597cb95162
MIDASIT\kjh1007 <kimjjun1007@gmail.com>
2023-02-09 17:42:13 +0900

    feat: splash screen btn

--- presentation/src/main/java/com/example/mitalk/ui/splash/SplashScreen.kt
+++ presentation/src/main/java/com/example/mitalk/ui/splash/SplashScreen.kt
@@ -1,16 +1,29 @@
 package com.example.mitalk.ui.splash
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.mitalk.util.miClickable
 import com.example.mitalk.util.theme.Medium21GM
+import com.example.mitalk.util.theme.MitalkColor
 import com.example.mitalk.util.theme.MitalkIcon
+import com.example.mitalk.util.theme.Regular12NO
+
+@Stable
+private val BtnShape = RoundedCornerShape(8.dp)
 
 @Composable
 fun SplashScreen() {
@@ -41,6 +54,37 @@ fun SplashScreen() {
         }
 
         Spacer(modifier = Modifier.height(69.dp))
+
+        Box(
+            modifier = Modifier
+                .padding(horizontal = 20.dp)
+                .fillMaxWidth()
+                .height(40.dp)
+                .background(
+                    color = MitalkColor.White,
+                    shape = BtnShape
+                )
+                .clip(shape = BtnShape)
+                .border(
+                    width = 2.dp,
+                    shape = BtnShape,
+                    color = Color(0xFFA69E9E)
+                )
+                .miClickable { },
+            contentAlignment = Alignment.Center,
+        ) {
+            Row {
+               Image(
+                   painter = painterResource(id = MitalkIcon.Google_Icon.drawableId),
+                   contentDescription = MitalkIcon.Google_Icon.contentDescription,
+                   modifier = Modifier.size(28.dp),
+               )
+                
+                Spacer(modifier = Modifier.width(10.dp))
+
+                Regular12NO(text = "Continue with Google")
+            }
+        }
 
     }
 }
