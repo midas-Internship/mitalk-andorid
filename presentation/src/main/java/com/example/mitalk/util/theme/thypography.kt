dd4518745c43b0ce2778516165a948b1b24c08d8
MIDASIT\bsm0417 <bsmin0417@naver.com>
2023-02-10 12:25:04 +0900

    feat: develop 반영

--- presentation/src/main/java/com/example/mitalk/util/theme/thypography.kt
+++ presentation/src/main/java/com/example/mitalk/util/theme/thypography.kt
@@ -1,6 +1,5 @@
 package com.example.mitalk.util.theme
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
@@ -11,7 +10,6 @@ import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.mitalk.R
 
