72519612a03d64b77d1100a5453a62fa84ebc20e
MIDASIT\bsm0417 <bsmin0417@naver.com>
2023-02-10 12:06:44 +0900

    feat: 상담연결 ui 퍼블리싱

---
+++ presentation/src/main/java/com/example/mitalk/ui/chat_type/ChatTypeScreen.kt
@@ -0,0 +1,123 @@
+package com.example.mitalk.ui.chat_type
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.mitalk.R
+import com.example.mitalk.ui.util.MiHeader
+import com.example.mitalk.util.theme.MitalkColor
+import com.example.mitalk.util.theme.Regular14NO
+import com.example.mitalk.util.theme.Regular7NO
+
+@Stable
+private val ChatTypeBoxShape = RoundedCornerShape(8.dp)
+
+@Composable
+fun ChatTypeScreen(
+    navController: NavController,
+) {
+    Column {
+        MiHeader(
+            navController = navController,
+            text = stringResource(id = R.string.consulting_connect)
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Row(modifier = Modifier.weight(1f)) {
+            Spacer(modifier = Modifier.width(20.dp))
+            Column(modifier = Modifier.weight(1f)) {
+                ChatTypeBox(
+                    title = stringResource(id = R.string.function_suggest),
+                    comment = stringResource(id = R.string.function_suggest_comment),
+                    modifier = Modifier
+                        .weight(7f)
+                        .background(color = Color(0xFF62A3A7), shape = ChatTypeBoxShape)
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                ChatTypeBox(
+                    title = stringResource(id = R.string.function_question),
+                    comment = stringResource(id = R.string.function_suggest_comment),
+                    modifier = Modifier
+                        .weight(5f)
+                        .background(color = Color(0xFF81A578), shape = ChatTypeBoxShape)
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                ChatTypeBox(
+                    title = stringResource(id = R.string.alliance_inquiry),
+                    comment = stringResource(id = R.string.alliance_inquiry_comment),
+                    modifier = Modifier
+                        .weight(6f)
+                        .background(color = Color(0xFF959FC3), shape = ChatTypeBoxShape)
+                )
+            }
+            Spacer(modifier = Modifier.width(20.dp))
+            Column(modifier = Modifier.weight(1f)) {
+                ChatTypeBox(
+                    title = stringResource(id = R.string.bug_report),
+                    comment = stringResource(id = R.string.bug_report_comment),
+                    modifier = Modifier
+                        .weight(5f)
+                        .background(color = Color(0xFFA96A6A), shape = ChatTypeBoxShape)
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                ChatTypeBox(
+                    title = stringResource(id = R.string.product_feedback),
+                    comment = stringResource(id = R.string.product_feedback_comment),
+                    modifier = Modifier
+                        .weight(7f)
+                        .background(color = Color(0xFFB49C79), shape = ChatTypeBoxShape)
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                ChatTypeBox(
+                    title = stringResource(id = R.string.etc),
+                    modifier = Modifier
+                        .weight(6f)
+                        .background(color = Color(0xFF698EAF), shape = ChatTypeBoxShape)
+                )
+            }
+            Spacer(modifier = Modifier.width(20.dp))
+        }
+        Spacer(modifier = Modifier.height(25.dp))
+    }
+}
+
+@Composable
+fun ChatTypeBox(
+    title: String,
+    comment: String? = null,
+    modifier: Modifier
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(start = 12.dp, top = 15.dp)
+    ) {
+        Column {
+            Regular14NO(text = title, color = MitalkColor.White, modifier = Modifier.wrapContentHeight())
+            if (comment != null) {
+                Regular7NO(
+                    text = comment,
+                    color = MitalkColor.White,
+                    modifier = Modifier.wrapContentHeight()
+                )
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun showChatTypeScreen() {
+    val navController = rememberNavController()
+    ChatTypeScreen(navController = navController)
+}
