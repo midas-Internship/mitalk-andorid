706a0d4f2979ec8008fb7eb37f64bc40b9d0eb3a
MIDASIT\kjh1007 <kimjjun1007@gmail.com>
2023-02-13 17:27:28 +0900

    feat: data source

---
+++ data/src/main/java/com/example/data/remote/datasource/RemoteQuestionDataSourceImpl.kt
@@ -0,0 +1,15 @@
+package com.example.data.remote.datasource
+
+import com.example.data.remote.api.QuestionApi
+import com.example.data.remote.response.toEntity
+import com.example.data.remote.util.miTalkApiCall
+import com.example.domain.entity.QuestionEntity
+import javax.inject.Inject
+
+class RemoteQuestionDataSourceImpl @Inject constructor(
+    private val questionApi: QuestionApi,
+): RemoteQuestionDataSource {
+    override suspend fun getQuestionList(): QuestionEntity = miTalkApiCall {
+        questionApi.getQuestionList().toEntity()
+    }
+}
