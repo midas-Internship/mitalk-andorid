3da3e4ecff329a1d2fe05d9cd66c12cc7dbe9a93
MIDASIT\bsm0417 <bsmin0417@naver.com>
2023-02-13 16:54:40 +0900

    feat: 구글 로그인 및 자동 로그인 기능 구현

--- presentation/src/main/java/com/example/mitalk/MainActivity.kt
+++ presentation/src/main/java/com/example/mitalk/MainActivity.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavType
 import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
