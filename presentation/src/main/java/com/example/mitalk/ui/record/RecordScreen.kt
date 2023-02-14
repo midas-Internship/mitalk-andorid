f4b24afa1f88c0f0d3147d41bffb3bdb5002c004
MIDASIT\bsm0417 <bsmin0417@naver.com>
2023-02-14 16:51:10 +0900

    refactor: recordItemType 파일로 분리

--- presentation/src/main/java/com/example/mitalk/ui/record/RecordScreen.kt
+++ presentation/src/main/java/com/example/mitalk/ui/record/RecordScreen.kt
@@ -1,45 +1,41 @@
 package com.example.mitalk.ui.record
 
-import android.util.Log
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.mitalk.AppNavigationItem
 import com.example.mitalk.DeepLinkKey
 import com.example.mitalk.R
+import com.example.mitalk.mvi.RecordState
 import com.example.mitalk.ui.util.MiHeader
+import com.example.mitalk.ui.util.RecordItemType
 import com.example.mitalk.util.miClickable
 import com.example.mitalk.util.theme.*
+import com.example.mitalk.vm.record.RecordViewModel
 
 @Composable
 fun RecordScreen(
     navController: NavController,
+    vm: RecordViewModel = hiltViewModel()
 ) {
 
-    val recordList = listOf(
-        RecordData("2023.04.14", stringResource(id = R.string.function_question), "백승민"),
-        RecordData("2023.02.17", stringResource(id = R.string.product_feedback), "백승민"),
-        RecordData("2023.02.16", stringResource(id = R.string.bug_report), "백승민"),
-        RecordData("2023.02.15", stringResource(id = R.string.function_suggest), "백승민"),
-        RecordData("2023.02.14", stringResource(id = R.string.alliance_inquiry), "백승민"),
-        RecordData("2023.02.14", stringResource(id = R.string.etc), "백승민"),
-        RecordData("2021.02.14", stringResource(id = R.string.consulting_record), "백승민"),
-    )
+    val container = vm.container
+    val state = container.stateFlow.collectAsState().value
+    val sideEffect = container.sideEffectFlow
 
     Column {
         MiHeader(
@@ -48,16 +44,10 @@ fun RecordScreen(
         )
         RecordList(
             navController = navController,
-            list = recordList
+            list = state.recordList
         )
     }
 }
-
-data class RecordData(
-    val date: String,
-    val title: String,
-    val counselor: String,
-)
 
 private fun returnIcon(index: Int): Int =
     when (index % 3) {
@@ -74,65 +64,42 @@ private fun dateToInt(date: String): Int =
         val day = date.substring(8..9)
 
         (year + month + day).toInt()
-    } catch (e: StringIndexOutOfBoundsException) { 0 }
-private fun returnLastDate(list: List<RecordData>, index: Int): Int =
+    } catch (e: StringIndexOutOfBoundsException) {
+        0
+    }
+
+private fun returnLastDate(list: List<RecordState.RecordData>, index: Int): Int =
     if (index == 0) 0
-    else dateToInt(list[index-1].date)
+    else dateToInt(list[index - 1].time)
 
 @Composable
 private fun RecordList(
     navController: NavController,
-    list: List<RecordData>,
+    list: List<RecordState.RecordData>,
 ) {
     LazyColumn {
-        itemsIndexed(list) {index, it ->
+        itemsIndexed(list) { index, it ->
             RecordItem(
-                date = it.date,
+                date = it.time,
                 icon = painterResource(id = returnIcon(index)),
-                title = it.title,
-                counselor = it.counselor,
+                type = it.type,
+                counselor = it.counsellorName,
                 lastDate = returnLastDate(list, index),
             ) {
                 navController.navigate(
                     route = AppNavigationItem.RecordDetail.route
-                            + DeepLinkKey.HEADER + it.title
+                            + DeepLinkKey.HEADER + it.type
                 )
             }
         }
     }
 }
 
-private enum class RecordItemType(
-    val textColor: Color,
-) {
-    QUESTION(
-        textColor = Color(0xFFD49393),
-    ),
-    FEEDBACK(
-        textColor = MitalkColor.MainBlue,
-    ),
-    BUG(
-        textColor = MitalkColor.MainGreen,
-    ),
-    SUGGEST(
-        textColor = MitalkColor.MainBrown,
-    ),
-    INQUIRY(
-        textColor = Color(0xFFA1A0DF),
-    ),
-    ETC(
-        textColor = Color(0xFFC294C2),
-    ),
-    ElSE(
-        textColor = MitalkColor.Black,
-    ),
-}
-
 @Composable
 private fun RecordItem(
     date: String,
     icon: Painter,
-    title: String,
+    type: String,
     counselor: String,
     lastDate: Int,
     onClicked: () -> Unit
@@ -140,16 +107,16 @@ private fun RecordItem(
     val thisDateInt = dateToInt(date)
     val dividerHeight =
         if (lastDate - thisDateInt > 1000) 100.dp
-        else if(lastDate - thisDateInt > 100) 50.dp
+        else if (lastDate - thisDateInt > 100) 50.dp
         else if (lastDate > thisDateInt) 25.dp
         else 10.dp
-    val type = when (title) {
-        stringResource(id = R.string.function_question) -> RecordItemType.QUESTION
-        stringResource(id = R.string.product_feedback) -> RecordItemType.FEEDBACK
-        stringResource(id = R.string.bug_report) -> RecordItemType.BUG
-        stringResource(id = R.string.function_suggest) -> RecordItemType.SUGGEST
-        stringResource(id = R.string.alliance_inquiry) -> RecordItemType.INQUIRY
-        stringResource(id = R.string.etc) -> RecordItemType.ETC
+    val item = when (type) {
+        RecordItemType.QUESTION.type -> RecordItemType.QUESTION
+        RecordItemType.FEEDBACK.type -> RecordItemType.FEEDBACK
+        RecordItemType.BUG.type -> RecordItemType.BUG
+        RecordItemType.SUGGEST.type -> RecordItemType.SUGGEST
+        RecordItemType.INQUIRY.type -> RecordItemType.INQUIRY
+        RecordItemType.ETC.type -> RecordItemType.ETC
         else -> RecordItemType.ElSE
     }
     Column {
@@ -182,8 +149,8 @@ private fun RecordItem(
             Spacer(modifier = Modifier.width(10.dp))
 
             Medium15GM(
-                text = title,
-                color = type.textColor,
+                text = stringResource(id = item.titleId),
+                color = item.textColor,
                 modifier = Modifier.width(88.dp)
             )
 
@@ -199,7 +166,6 @@ private fun RecordItem(
             )
         }
     }
-
 }
 
 @Composable
