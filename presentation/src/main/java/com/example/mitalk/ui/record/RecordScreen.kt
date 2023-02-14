0122bc0ad523ee02a6208b46d7a6309140ed9762
MIDASIT\kjh1007 <kimjjun1007@gmail.com>
2023-02-14 10:02:01 +0900

    feat: record item type

--- presentation/src/main/java/com/example/mitalk/ui/record/RecordScreen.kt
+++ presentation/src/main/java/com/example/mitalk/ui/record/RecordScreen.kt
@@ -1,12 +1,17 @@
 package com.example.mitalk.ui.record
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.mitalk.R
 import com.example.mitalk.ui.util.MiHeader
+import com.example.mitalk.util.theme.MitalkColor
+import com.example.mitalk.util.theme.MitalkIcon
 
 @Composable
 fun RecordScreen(
@@ -16,6 +21,39 @@ fun RecordScreen(
         navController = navController,
         text = stringResource(id = R.string.consulting_record)
     )
+}
+
+private enum class RecordItemType(
+    val textColor: Color,
+) {
+    QUESTION(
+        textColor = Color(0xFFD49393),
+    ),
+    FEEDBACK(
+        textColor = MitalkColor.MainBlue,
+    ),
+    BUG(
+        textColor = MitalkColor.MainGreen,
+    ),
+    PROPOSAL(
+        textColor = MitalkColor.MainBrown,
+    ),
+    INQUIRY(
+        textColor = Color(0xFFA1A0DF)
+    ),
+    ETC(
+        textColor = Color(0xFFC294C2)
+    ),
+}
+
+@Composable
+fun RecordItem(
+    date: String,
+    title: String,
+    counselor: String,
+    icon: Painter,
+) {
+    
 }
 
 @Composable
