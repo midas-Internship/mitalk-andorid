17c2c1be6a461b81d6a57aeea557b5d4b479668b
MIDASIT\bsm0417 <bsmin0417@naver.com>
2023-02-15 10:44:02 +0900

    feat: postReview 로직 작성

--- data/src/main/java/com/example/data/remote/datasource/RemoteReviewDataSourceImpl.kt
+++ data/src/main/java/com/example/data/remote/datasource/RemoteReviewDataSourceImpl.kt
@@ -1,9 +1,15 @@
 package com.example.data.remote.datasource
 
 import com.example.data.remote.api.ReviewApi
+import com.example.data.remote.request.toRequest
+import com.example.data.remote.util.miTalkApiCall
+import com.example.domain.param.ReviewParam
 import javax.inject.Inject
 
 class RemoteReviewDataSourceImpl @Inject constructor(
     private val reviewApi: ReviewApi
 ) : RemoteReviewDataSource {
+    override suspend fun postReview(reviewParam: ReviewParam) = miTalkApiCall {
+        reviewApi.postReview(reviewParam.toRequest())
+    }
 }
