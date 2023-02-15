e944f81b511ad3eb5f48d4126a702ada6cf58609
MIDASIT\bsm0417 <bsmin0417@naver.com>
2023-02-15 10:44:02 +0900

    feat: postReview 로직 작성

---
+++ data/src/main/java/com/example/data/remote/request/ReviewRequest.kt
@@ -0,0 +1,19 @@
+package com.example.data.remote.request
+
+import com.example.domain.param.ReviewParam
+import com.google.gson.annotations.SerializedName
+
+data class ReviewRequest(
+    @SerializedName("star")
+    val star: Int,
+    @SerializedName("message")
+    val message: String?,
+    @SerializedName("review_item")
+    val reviewItem: List<String>
+)
+
+fun ReviewParam.toRequest() = ReviewRequest(
+    star = star,
+    message = message,
+    reviewItem = reviewItem
+)
